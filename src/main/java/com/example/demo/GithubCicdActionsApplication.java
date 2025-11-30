97123268663d86b891bd337f8c18cd347a5d64c4
mahendra <mahendrasing0115@gmail.com>
2025-11-30 12:35:25 +0530

    Code

---
+++ src/main/java/com/example/demo/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to springboot demo !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
